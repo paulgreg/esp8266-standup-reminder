76abb1b4159f0c880c7db084a672b875b8b84485
Grégory Paul <me@gpaul.me>
2022-01-03 18:59:36 +0100

    Connecting now to USB instead of battery...
    so electricity consumption is not a concern anymore : lit LED more often

--- esp8266-standup-reminder.ino
+++ esp8266-standup-reminder.ino
@@ -1,9 +1,13 @@
 #define LED 15
-#define BUTTON 13
 
 #define SECOND 1000
 #define MINUTE 60 * SECOND
 #define HOUR 60 * MINUTE
+
+#define ALARM_1 1 * HOUR
+#define ALARM_2 2 * HOUR
+#define SLEEP   3 * HOUR
+
 
 void blink(int d) {
   digitalWrite(LED, HIGH);
@@ -17,7 +21,8 @@ void blinkRepeat(int nb, int d) {
 }
 
 void setup() {
-  pinMode(BUTTON, INPUT);
+  Serial.begin(115200);
+  Serial.println("Standup Reminder");
   pinMode(LED, OUTPUT);
   blinkRepeat(5, 100);
 }
@@ -26,18 +31,37 @@ int state = 0;
 int minutes = 0;
 
 void loop() {
-  minutes++;
+  Serial.printf("minutes: %i\n", minutes);
   if (state == 0) {
-    delay(MINUTE - SECOND);
-    if (minutes < 60) blink(SECOND);
-    else state = 1;
-  }
-  if (state == 1) {
-    if (minutes < 120) blinkRepeat(60, SECOND);
-    else state = 2;
-  }
-  if (state == 2) {
+    if (minutes < ALARM_1) {
+      Serial.println("blink one second");
+      blink(SECOND);
+      delay(MINUTE - SECOND);
+    } else {
+      Serial.println("set state 1");
+      state = 1;
+    }
+  } else if (state == 1) {
+    if (minutes < ALARM_2) {
+      Serial.println("turn on LED");
+      digitalWrite(LED, HIGH);
+      delay(MINUTE);
+    } else {
+      Serial.println("set state 2");
+      state = 2;
+    }
+  } else if (state == 2) {
+     if (minutes < SLEEP) {
+      Serial.println("blink for one minute");
+      blinkRepeat(60, SECOND);
+    } else {
+      Serial.println("set state 3");
+      state = 3;
+    }
+  } else if (state == 3) {
+     Serial.println("state 3 - sleeping now");
      digitalWrite(LED, LOW);
      ESP.deepSleep(0);
   }
+  minutes++;
 }
